1340fd3c074847945ba3e975a4ba1a2f650d12c7
abulatova <bulatovaalsu22@gmail.com>
2026-02-14 19:02:31 +0300

    remove old

--- client/src/entities/board/index.ts
+++
@@ -1,5 +0,0 @@
-export { BoardCanvas } from './BoardCanvas';
-export { BoardCanvasNew } from './BoardCanvasNew';
-export { CameraContext, useCameraContext } from './CameraContext';
-export { useBoardShapes } from './useBoardShapes';
-export { useGridSystem } from './useGridSystem';
